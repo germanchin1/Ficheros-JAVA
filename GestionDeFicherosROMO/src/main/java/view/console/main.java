9e2395746b344b61d2d3585c7ed89d254cf35bdb
germanchin1 <germanromo2006@gmail.com>
2025-04-29 10:16:15 +0200

    subida de ejercicio de ficheros JAVA

---
+++ GestionDeFicherosROMO/src/main/java/view/console/main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.main to edit this template
+ */
+package view.console;
+
+/**
+ *
+ * @author germanromort
+ */
+public class main {
+    public static void main(String[] args) {
+        
+    }
+}
